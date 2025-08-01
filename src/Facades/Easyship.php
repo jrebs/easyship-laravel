297d4a6edf0cff0869e4541a1832a6613be225c5
Rob Pungello <rob@rpun.io>
2025-08-01 16:45:48 -0400

    Add @mixin DocBlock for facade (#3)
    This allows IDEs like PhpStorm to correctly provide code-completion for facade calls

--- src/Facades/Easyship.php
+++ src/Facades/Easyship.php
@@ -2,8 +2,10 @@
 
 namespace Easyship\Facades;
 
+use Easyship\EasyshipAPI;
 use Illuminate\Support\Facades\Facade;
 
+/** @mixin EasyshipAPI */
 class Easyship extends Facade
 {
     protected static function getFacadeAccessor()
